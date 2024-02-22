1cc79dc5b5dccb6756310a47a957d0501ab9eafe
prajadiputra22 <darmawan.prajadiputra_ti22@nusaputra.ac.id>
2024-02-22 21:38:06 +0700

    commit

---
+++ Tugas2/Class.java
@@ -0,0 +1,46 @@
+package Tugas2;
+
+public class Class {
+    public static void main(String[] args) {
+        Triangle triangle = new Triangle(10,5);
+
+        // triangle.setAlas(4);
+        // triangle.setTinggi(5);
+        // triangle.setLuas();
+
+        int hasil = triangle.getLuas();
+        System.out.println("Hasil dari luas segitga adalah : " + hasil);
+    }
+}
+
+class Triangle {
+
+    private int alas;
+    private int tinggi;
+
+    //CONSTRUCTOR
+    public Triangle(int alas, int tinggi) {
+        this.tinggi = tinggi;
+        this.alas = alas;
+    }
+    
+    //SETTER
+    public void setAlas (int value) {
+        this.alas = value;
+    }
+
+    public void setTinggi (int value) {
+        this.tinggi = value;
+    }
+
+    // public void setLuas () {
+    //     int luas = this.alas * this.tinggi / 2;
+    //     System.out.println(luas);
+    // }
+
+    //GETTER
+    public int getLuas () {
+        return this.alas * this.tinggi / 2;
+    }
+}
+
