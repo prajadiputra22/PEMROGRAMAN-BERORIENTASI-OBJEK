785e8a566680bdb46d7959ffe9990cadaa0c0412
prajadiputra22 <darmawan.prajadiputra_ti22@nusaputra.ac.id>
2024-02-22 22:31:59 +0700

    Commit

--- Tugas1/Segitiga.java
+++ Tugas1/Segitiga.java
@@ -6,8 +6,7 @@ public class Segitiga {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
-        System.out.println("=== Hitung Luas dan Keliling Segitiga Sembarang ===");
-
+        System.out.println("=== Hitung Keliling Segitiga Sembarang ===");
         System.out.print("Masukan panjang sisi A segitiga : ");
         double a = scanner.nextDouble();
 
@@ -17,17 +16,19 @@ public class Segitiga {
         System.out.print("Masukan panjang sisi C segitiga : ");
         double c= scanner.nextDouble();
 
+        double keliling = a + b + c;
+        System.out.println('\n'+"Keliling Segitiga : " + keliling + " cm" + '\n');
+
+
+        System.out.println("=== Hitung Luas Segitiga Sembarang ===");
         System.out.print("Masukan panjang alas segitiga : ");
         double alas = scanner.nextDouble();
 
         System.out.print("Masukan tinggi segitiga : ");
         double tinggi = scanner.nextDouble();
-
-        double keliling = a + b + c;
+        
         double luas = alas * tinggi / 2;
-
-        System.out.println("Keliling Segitiga : " + keliling + " cm");
-        System.out.println("Luas segita : " + luas + " cm");
+        System.out.println('\n'+"Luas Segita : " + luas + " cm" + '\n');
 
         scanner.close();
     }
